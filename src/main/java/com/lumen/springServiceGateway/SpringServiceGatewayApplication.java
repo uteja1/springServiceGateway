30c705b478c04768c55b4e89e5ce2ce1a4c4ba76
Uday Teja k <uteja70@gmail.com>
2022-07-07 19:38:14 +0530

    initial commit

---
+++ src/main/java/com/lumen/springServiceGateway/SpringServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lumen.springServiceGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lumen/springServiceGateway/SpringServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumen.springServiceGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
